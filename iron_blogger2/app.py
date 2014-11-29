c38bd1aa426b5dd8a1101d0c70db5f6f717fab29
Ian Denhardt <ian@zenhack.net>
2014-11-28 23:20:42 -0500

    Fetch feeds and store posts in the db (WIP).
    We're only storing the dates thus far, and we aren't dealing with
    duplicate posts. Also, importing all of the feeds takes a while; some
    optimizations would not go amiss.

--- iron_blogger2/app.py
+++ iron_blogger2/app.py
@@ -13,12 +13,20 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>
+from datetime import date
+import time
+
 from flask import Flask, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+import feedparser
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///iron-blogger.db'
 db = SQLAlchemy(app)
+
+
+class MalformedPostError(Exception):
+    """Raised when parsing a post fails."""
 
 
 class Blogger(db.Model):
@@ -50,12 +58,48 @@ class Blog(db.Model):
         if blogger is not None:
             self.blogger = blogger
 
+    def sync_posts(self):
+        feed = feedparser.parse(self.feed_url)
+        for entry in feed.entries:
+            # FIXME: we need a way of de-duping posts which are already in the
+            # database.
+            self.posts.append(Post.from_feed_entry(entry))
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     blog_id = db.Column(db.Integer, db.ForeignKey('blog.id'), nullable=False)
     date = db.Column(db.Date, nullable=False)
     blog = db.relationship('Blog', backref=db.backref('posts'))
+
+    @staticmethod
+    def _get_pub_date(feed_entry):
+        """Return a datetime.date object for the post's publication date.
+
+        ``feed_entry`` should be a post object as returned by
+        ``feedparser.parse``.
+
+        If the post does not have a publication date, raise a
+        ``MalformedPostError``.
+        """
+        for key in 'published', 'created', 'updated':
+            key += '_parsed'
+            if key in feed_entry:
+                return date.fromtimestamp(time.mktime(feed_entry[key]))
+        raise MalformedPostError("No publication date in post: %r" %
+                                 feed_entry)
+
+    @staticmethod
+    def from_feed_entry(entry):
+        """Read and construct Post object from ``entry``.
+
+        ``entry`` should be a post object as returned by ``feedparser.parse``.
+
+        If the post is invalid, raise a ``MalformedPostError`.
+        """
+        post = Post()
+        post.date = Post._get_pub_date(entry)
+        return post
 
 
 @app.route('/bloggers')
