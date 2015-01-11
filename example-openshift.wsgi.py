a0bd5460afd7e5404a5cfcf695decd3763cedd92
Ian Denhardt <ian@zenhack.net>
2015-01-10 21:04:39 -0500

    Openshift example, and some helpful notes re: extra deps.

---
+++ example-openshift.wsgi.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# wsgi script configured to run under redhat's openshift, using postgresql for
+# the database.
+#
+# To use this, copy it to wsgi.py edit, APPNAME (below) if need be, uncomment
+# the dependency for postgresql in requirements.txt, commit, and push. you'll
+# have to arrange for the initial population of the database yourself, as well
+# as syncing posts. In the future, we'll have a more polished answer for this.
+
+# This bit is copied from openshift's standard python boilerplate:
+import os
+
+virtenv = os.environ['OPENSHIFT_PYTHON_DIR'] + '/virtenv/'
+virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+try:
+    execfile(virtualenv, dict(__file__=virtualenv))
+except IOError:
+    pass
+
+# Okay, the rest of this is all us, and just standard wsgi.
+
+APPNAME = 'ironblogger' # The name of our app on openshift.
+
+from iron_blogger2.config import setup
+from iron_blogger2.app import app as application
+
+setup({
+    'db_uri': os.getenv('OPENSHIFT_POSTGRESQL_DB_URL') + '/' + APPNAME,
+})
