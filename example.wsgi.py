5f4d694cf1cb3db00460060fcffe2c6ee97e231c
Ian Denhardt <ian@zenhack.net>
2015-01-10 22:08:38 -0500

    Update outdated comment.
    postgresql has also been tested.

--- example.wsgi.py
+++ example.wsgi.py
@@ -4,7 +4,7 @@ from iron_blogger2.app import app as application
 
 # Example config; tune to taste.
 setup({
-    # The database to use. Any SQLAlchemy URI can be used, but only SQLite has
-    # been tested, and other DBMSes may not work:
+    # The database to use. Any SQLAlchemy URI can be used, but only SQLite and
+    # postgresql have tested, and other DBMSes may not work:
     'db_uri': 'sqlite:////path/to/sql.db',
 })
