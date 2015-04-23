f8c4f613aef4b251f290e5237d4ffb1f42018593
Ian Denhardt <ian@zenhack.net>
2015-04-23 00:20:59 -0400

    Refactor assign_posts tests into proper TestCase
    ...with a setUp and tearDown, which will be nice when I add more tests.

--- tests/test_assign_posts.py
+++ tests/test_assign_posts.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import unittest
 
 from ironblogger.model import db, Blogger, Blog, BloggerRound, Post
 from ironblogger.app import app
@@ -6,62 +7,73 @@ from ironblogger.wsgi import setup
 from ironblogger.date import duedate
 
 
-with app.test_request_context():
-    setup({
-        'region'  : 'Boston',
-        'timezone': '-0500',
-        'language': 'en-us',
-        'db_uri'  : 'sqlite:///:memory:',
-    })
-    db.create_all()
+def _get_week(when):
+    return db.session.query(BloggerRound)\
+        .filter_by(due=duedate(when)).one()
 
-    # During a 5-week period, alice:
-    #
-    # * Posts on time in the first week
-    # * Misses week 2
-    # * Posts twice in week 3
-    # * Misses weeks 4 and 5
-    alice = Blogger(name='Alice',
-                    start_date=datetime(2015, 4, 1),
-                    blogs=[
-                        Blog(title='Fun with crypto',
-                             page_url='http://example.com/alice/blog.html',
-                             feed_url='http://example.com/alice/rss.xml',
-                             posts=[
-                                 Post(timestamp=datetime(2015, 4, 1),
-                                      title="BREAKING: P = NP; Asymmetric crypto broken forever :(",
-                                      summary="The title says it all. There are many other "
-                                              "implications of this, but given the topic of...",
-                                      page_url='http://example.com/alice/april-fools-pnp.html',
-                                      ),
-                                 Post(timestamp=datetime(2015, 4, 15),
-                                      title="Security Breach",
-                                      summary="Sorry I didn't post last week. There was a security "
-                                              "breach at work, and I was busy dealing with...",
-                                      page_url='http://example.com/alice/security-breach.html',
-                                      ),
-                                 Post(timestamp=datetime(2015, 4, 16),
-                                      title="Javascript and timing attacks",
-                                      summary="Owing to it being the only thing available in the "
-                                              "browser, people are doing more and more with Javascript...",
-                                      page_url='http:///example.com/alice/javascript-timing-attacks.html',
-                                      ),
-                                ]
-                            )])
-    db.session.add(alice)
 
-    end_date = datetime(2015, 4, 28)
-    alice.create_rounds(end_date)
-    alice.assign_posts(end_date)
+class Test_assign_posts(unittest.TestCase):
 
-    def _get_week(when):
-        return db.session.query(BloggerRound)\
-            .filter_by(due=duedate(when)).one()
+    def setUp(self):
+        self.ctx = app.test_request_context()
+        self.ctx.push()
+        setup({
+            'region'  : 'Boston',
+            'timezone': '-0500',
+            'language': 'en-us',
+            'db_uri'  : 'sqlite:///:memory:',
+        })
+        db.create_all()
 
-    assert "BREAKING:" in _get_week(datetime(2015, 4, 1)).post.title
-    assert "Security Breach" == _get_week(datetime(2015, 4, 8)).post.title
-    assert "Javascript" in _get_week(datetime(2015, 4, 15)).post.title
-    assert _get_week(datetime(2015, 4, 22)).post is None
+        # During a 5-week period, alice:
+        #
+        # * Posts on time in the first week
+        # * Misses week 2
+        # * Posts twice in week 3
+        # * Misses weeks 4 and 5
+        alice = Blogger(name='Alice',
+                        start_date=datetime(2015, 4, 1),
+                        blogs=[
+                            Blog(title='Fun with crypto',
+                                page_url='http://example.com/alice/blog.html',
+                                feed_url='http://example.com/alice/rss.xml',
+                                posts=[
+                                    Post(timestamp=datetime(2015, 4, 1),
+                                        title="BREAKING: P = NP; Asymmetric crypto broken forever :(",
+                                        summary="The title says it all. There are many other "
+                                                "implications of this, but given the topic of...",
+                                        page_url='http://example.com/alice/april-fools-pnp.html',
+                                        ),
+                                    Post(timestamp=datetime(2015, 4, 15),
+                                        title="Security Breach",
+                                        summary="Sorry I didn't post last week. There was a security "
+                                                "breach at work, and I was busy dealing with...",
+                                        page_url='http://example.com/alice/security-breach.html',
+                                        ),
+                                    Post(timestamp=datetime(2015, 4, 16),
+                                        title="Javascript and timing attacks",
+                                        summary="Owing to it being the only thing available in the "
+                                                "browser, people are doing more and more with Javascript...",
+                                        page_url='http:///example.com/alice/javascript-timing-attacks.html',
+                                        ),
+                                    ]
+                                )])
+        db.session.add(alice)
+        self.alice = alice
 
-    # Just make sure this doesn't explode:
-    alice.assign_posts()
+    def tearDown(self):
+        self.ctx.pop()
+
+    def test_blogger_method(self):
+        """Test the assign_posts method of the Blogger class"""
+        end_date = datetime(2015, 4, 28)
+        self.alice.create_rounds(end_date)
+        self.alice.assign_posts(end_date)
+
+        assert "BREAKING:" in _get_week(datetime(2015, 4, 1)).post.title
+        assert "Security Breach" == _get_week(datetime(2015, 4, 8)).post.title
+        assert "Javascript" in _get_week(datetime(2015, 4, 15)).post.title
+        assert _get_week(datetime(2015, 4, 22)).post is None
+
+        # Just make sure this doesn't explode:
+        self.alice.assign_posts()
