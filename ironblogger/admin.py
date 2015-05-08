d8a3cb51f81636d5237d862084c0ac4f988a1d14
Ian Denhardt <ian@zenhack.net>
2015-05-07 22:45:57 -0400

    Add a bit more to admin panel

--- ironblogger/admin.py
+++ ironblogger/admin.py
@@ -1,10 +1,31 @@
 from flask.ext.admin import Admin, BaseView, expose
+from flask.ext.admin.contrib.sqla import ModelView
+from flask.ext import login
+from ironblogger import model
 
-class MainView(BaseView):
+class AdminViewMixin(object):
+    """A mixin class which restricts access to admin users."""
+
+    def is_accessible(self):
+        return login.current_user.is_authenticated() and login.current_user.is_admin
+
+
+class MainView(AdminViewMixin, BaseView):
 
     @expose('/')
     def index(self):
         return self.render('admin-test.html')
 
+
+class AdminModelView(AdminViewMixin, ModelView):
+    pass
+
+
+class UserView(AdminModelView):
+
+    column_list = ('name', 'is_admin')
+
+
 admin = Admin()
 admin.add_view(MainView(name='test'))
+admin.add_view(UserView(model.User, model.db.session))
