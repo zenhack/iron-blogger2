7e665c84674e418df5dbc7dbd64b000adbde65b6
Ian Denhardt <ian@zenhack.net>
2015-05-31 16:10:32 -0400

    Hack around double initialization of flask admin

--- ironblogger/wsgi.py
+++ ironblogger/wsgi.py
@@ -4,10 +4,16 @@ from ironblogger.model import db as _db
 from ironblogger import route as _route
 from ironblogger.admin import admin as _admin
 
+_setup_already = False
 
 def setup(cfg):
     config.setup(cfg)
     application.config['SQLALCHEMY_DATABASE_URI'] = config.cfg['db_uri']
     application.secret_key = config.cfg['app_secret_key']
     _db.init_app(application)
-    _admin.init_app(application)
+    global _setup_already
+    if not _setup_already:
+        _admin.init_app(application)
+        # Flask admin seems to really disklike being initialized twice,
+        # as sometimes happens in the test suite.
+        _setup_already = True
