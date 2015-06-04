add65374c7c509b75624c95d0088cb9f935a8924
jywarren <jeff@unterbahn.com>
2015-06-04 15:15:43 -0700

    more styling & layout tweaks, attempt to avoid setup pitfall in wsgi example

--- example.wsgi.py
+++ example.wsgi.py
@@ -8,7 +8,7 @@ setup({
     'language': 'en-us',
     # The database to use. Any SQLAlchemy URI can be used, but only SQLite and
     # postgresql have tested, and other DBMSes may not work:
-    'db_uri': 'sqlite:////path/to/sql.db',
+    'db_uri': 'sqlite:///path/to/sql.db',
     # Secret key used for things like storing session information.
     # You can generate a key by running:
     #   dd if=/dev/random bs=1 count=128 | base64
