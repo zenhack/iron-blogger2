445892ef0eb2ceb19833b93d9f90918442bdec4f
Ian Denhardt <ian@zenhack.net>
2015-09-09 21:33:56 -0400

    Test views after assigning rounds

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,7 +1,12 @@
+"""Crawl the web interface, making sure pages return correct status codes.
+
+So far, we're only testing pages that we know should always return 200.
+"""
 import pytest
 
+from ironblogger import tasks
 from ironblogger.app import app
-from ironblogger.model import db
+from ironblogger.model import db, Post
 from .util import fresh_context, example_databases
 fresh_context = pytest.yield_fixture(autouse=True)(fresh_context)
 
@@ -31,8 +36,8 @@ pages = (
 
 
 @pytest.mark.parametrize('page', pages)
-def test_empty_page_ok(client, page):
-    """Each of these pages should return successfully."""
+def test_empty_db_ok(client, page):
+    """Crawl the website with an empty database."""
     resp = client.get(page)
     assert resp.status_code == 200
 
@@ -40,8 +45,21 @@ def test_empty_page_ok(client, page):
 @pytest.mark.parametrize('page,database', [(p, d)
                                            for p in pages
                                            for d in example_databases])
-def test_db_page_ok(client, page, database):
+def test_populated_db_page_ok(client, page, database):
+    """Crawl the website with a populated database.
+
+    This tests checks each page both before and after assigning rounds
+    to posts.
+    """
     db.session.add(database())
     db.session.commit()
+
+    resp = client.get(page)
+    assert resp.status_code == 200
+
+    last_post = db.session.query(Post.timestamp)\
+        .order_by(Post.timestamp.desc())\
+        .first()[0]
+    tasks.assign_rounds(until=last_post)
     resp = client.get(page)
     assert resp.status_code == 200
