zenhack/iron-blogger2

commit d6795017c49ccc0298395bf48c9b8452042e2aa5

Bugfix: assign_rounds needs to handle possibly zero bloggers.
Caught by our new shiny randomized test :P.

ironblogger/tasks.py
@@ -42,7 +42,11 @@ def assign_rounds(since=None, until=None):
         until = datetime.utcnow()
     if since is None:
         since = db.session.query(Blogger.start_date)\
-                          .order_by(Blogger.start_date).first()[0]
+                          .order_by(Blogger.start_date).first()
+    if since is None:
+        # If this is *still* true, there are no bloggers in the database; we're
+        # done!
+        return
 
     posts = db.session.query(Post)\
         .filter(Post.counts_for == None,

commit 2f32b4790f9670fe8f5b8b3233b2ae8d3a644af1

Split up tests/util
Randomized helpers and example data are now in their own files. Some of
the import statements in tests have been changes to accommodate this.

This also includes a fix to a bug introduced by
d6795017c49ccc0298395bf48c9b8452042e2aa5. It ought to be it's own
commit, but right now I just cannot be bothered. Basically: I
inadvertently removed the bit were we take the column out of our result,
so somewhere we ended up passing in a tuple where we wanted a datetime.

ironblogger/tasks.py
@@ -43,10 +43,12 @@ def assign_rounds(since=None, until=None):
     if since is None:
         since = db.session.query(Blogger.start_date)\
                           .order_by(Blogger.start_date).first()
-    if since is None:
-        # If this is *still* true, there are no bloggers in the database; we're
-        # done!
-        return
+        if since is None:
+            # If this is *still* true, there are no bloggers in the database;
+            # we're done!
+            return
+        # Rows are returned as tuples; we want the raw value:
+        since = since[0]
 
     posts = db.session.query(Post)\
         .filter(Post.counts_for == None,

tests/test_assign_rounds.py
@@ -1,7 +1,8 @@
 from datetime import datetime
 import unittest
 import pytest
-from tests.util import fresh_context, example_databases
+from .util import fresh_context
+from .util.example_data import databases as example_databases
 
 from ironblogger.model import db, Blogger, Blog, Post
 from ironblogger.app import app

tests/test_view.py
@@ -12,7 +12,9 @@ from ironblogger import tasks
 from ironblogger.app import app
 from ironblogger.model import db, Post
 from ironblogger.date import to_dbtime
-from .util import fresh_context, example_databases, random_database
+from .util.example_data import databases as example_databases
+from .util.randomize import random_database
+from .util import fresh_context
 fresh_context = pytest.yield_fixture(autouse=True)(fresh_context)
 
 

tests/util/__init__.py
@@ -1,8 +1,14 @@
-from ironblogger.app import app
-from ironblogger.model import db, Blogger, Blog, Post
-from ironblogger.date import to_dbtime
+"""This package provides helpers for use in tests.
+
+In addition to the contents of the root module, there are two other
+modules in this package:
+
+    * randomize, which provides helpers for randomize testing
+    * example_data, which provides example data for use in tests.
+"""
+from ironblogger.app import app, db
+from ironblogger.model import Blogger, Blog
 from datetime import datetime
-from random import Random
 import tempfile
 import os
 
@@ -45,110 +51,3 @@ def feedtext_to_blog(feedtext):
         # be used by the tests.
         os.remove(name)
         raise
-
-
-def random_database(seed, now):
-    """Generate a random database based on random seed `seed`.
-
-    This expects to be run within an application context; after the call the
-    objects will be stored in the associated datbase.
-
-    Note that this function does not commit the changes to the database; the
-    caller must do that themselves.
-    """
-    rand = Random(seed)
-
-    blogger_choices = ['Alice', 'Bob', 'Cindy', 'Dave', 'Eve', 'Fred', 'Gina',
-                       'Henry', 'Isabelle', 'Joe', 'Kate', 'Larry', 'Mallory',
-                       'Nina', 'Owen', 'Patty', 'Quentin', 'Ron', 'Sarah',
-                       'Tim', 'Uwe', 'Vincent', 'Wendy', 'Xavier', 'Yolanda',
-                       'Zack']
-
-    num_bloggers = rand.randint(0, len(blogger_choices) - 1)
-    blogger_names = rand.sample(blogger_choices, num_bloggers)
-
-    first_start_date = now.replace(weeks=-8)
-    last_start_date = now.replace(weeks=+8)
-    for name in blogger_names:
-        start_date = random_arrow(rand, first_start_date, last_start_date)
-        db.session.add(Blogger(name=name,
-                               start_date=to_dbtime(start_date)))
-
-
-def random_arrow(rand, start, end):
-    """Generate a random arrow object in the range [start,end].
-
-    `rand` is an instance of `Random`.
-    """
-    offset = rand.randint(0, (end - start).total_seconds())
-    return start.replace(seconds=offset)
-
-
-# Example databases; often useful. If we re-use existing objects the tests may
-# interfere with one another. By wrapping each of these in a lambda, we ensure
-# that a fresh copy is used each time.
-example_databases = [
-    lambda: Blogger(name='Alice',
-                    start_date=datetime(2015, 4, 1),
-                    blogs=[
-                        Blog(title='Fun with crypto',
-                            page_url='http://example.com/alice/blog.html',
-                            feed_url='http://example.com/alice/rss.xml',
-                            posts=[
-                                Post(timestamp=datetime(2015, 4, 1),
-                                    title="BREAKING: P = NP; Asymmetric crypto broken forever :(",
-                                    summary="The title says it all. There are many other "
-                                            "implications of this, but given the topic of...",
-                                    page_url='http://example.com/alice/april-fools-pnp.html',
-                                    ),
-                                Post(timestamp=datetime(2015, 4, 15),
-                                    title="Security Breach",
-                                    summary="Sorry I didn't post last week. There was a security "
-                                            "breach at work, and I was busy dealing with...",
-                                    page_url='http://example.com/alice/security-breach.html',
-                                    ),
-                                Post(timestamp=datetime(2015, 4, 16),
-                                    title="Javascript and timing attacks",
-                                    summary="Owing to it being the only thing available in the "
-                                            "browser, people are doing more and more with Javascript...",
-                                    page_url='http://example.com/alice/javascript-timing-attacks.html',
-                                    ),
-                                ]
-            )]),
-    lambda: Blogger(name='bob',
-                    start_date=datetime(2015, 4, 1),
-                    blogs=[
-                        Blog(title='Theoretical piracy',
-                            page_url='http://example.com/bob/blog.html',
-                            feed_url='http://example.com/bob/blog.atom',
-                            posts=[
-                                Post(title="Rivest admits he doesn't care, about security, just really likes prime numbers.",
-                                    summary="""In an interview, Robert Rivest told reporters that "I'm really only into it for the math...""",
-                                    page_url='http://example.com/bob/rivest.html',
-                                    timestamp=datetime(2015, 4, 1),
-                                    ),
-                                Post(title="Real world cryptographic hash functions have no theoretical basis, and we're all doomed",
-                                    summary="We're using cryptographic hash functions like sha256 as ideal hash functions. Ideal hash "
-                                            "functions provably do not exist. This in itself isn't actually that scary, but the really "
-                                            "worrysome thing is that we don't even have a well defined notion of what's 'good enough'; "
-                                            "What's the approximation that we're actually relying on?",
-                                    page_url='http://example.com/bob/hash-functions.html',
-                                    timestamp=datetime(2015, 4, 8),
-                                    ),
-                                Post(title="Timing sidechannels ruin everything, and we have no answers whatsoever ",
-                                    summary="There's a known attack on SSH that's been around for 15 years, that allows "
-                                            "an attacker to sniff passwords by measuring the timing of a user's keystrokes --- "
-                                            "no decryption required. Nobody's done anything about it, and while we have some ideas "
-                                            "about how to deal with this one, there's a whole class of attacks out there that we "
-                                            "just have no answer for.",
-                                    page_url='http://example.com/bob/timing-sidechannels.html',
-                                    timestamp=datetime(2015, 4, 9),
-                                    ),
-                                Post(title="I've realized I'm suffering from depression.",
-                                    summary="You may have noticed my past few posts were especially dark...",
-                                    page_url='http://example.com/bob/depressed.html',
-                                    timestamp=datetime(2015, 4, 15),
-                                    ),
-                            ]
-            )]),
-]

tests/util/example_data.py
@@ -1,0 +1,83 @@
+"""This module provides example data to use in tests.
+
+Right now this contains just the variable `databases`, which i a list of
+callables, each of which takes no arguments and returns an object which may
+be added to the database with `db.session.add`. Doing so will pull in the rest
+of the sample database's objects via dependency.
+
+NOTE WELL: some of the tests depend on specific knowledge of the contents of,
+these databases, so they should not be changed willy-nilly.
+"""
+from ironblogger.model import Blogger, Blog, Post
+from datetime import datetime
+
+# If we re-use existing objects the tests may interfere with one another. By
+# wrapping each of these in a lambda, we ensure that a fresh copy is used each time.
+#
+# TODO: we ought to convert this to a dict with memorable keys that describe
+# the contents, or something similar.
+databases = [
+    lambda: Blogger(name='Alice',
+                    start_date=datetime(2015, 4, 1),
+                    blogs=[
+                        Blog(title='Fun with crypto',
+                            page_url='http://example.com/alice/blog.html',
+                            feed_url='http://example.com/alice/rss.xml',
+                            posts=[
+                                Post(timestamp=datetime(2015, 4, 1),
+                                    title="BREAKING: P = NP; Asymmetric crypto broken forever :(",
+                                    summary="The title says it all. There are many other "
+                                            "implications of this, but given the topic of...",
+                                    page_url='http://example.com/alice/april-fools-pnp.html',
+                                    ),
+                                Post(timestamp=datetime(2015, 4, 15),
+                                    title="Security Breach",
+                                    summary="Sorry I didn't post last week. There was a security "
+                                            "breach at work, and I was busy dealing with...",
+                                    page_url='http://example.com/alice/security-breach.html',
+                                    ),
+                                Post(timestamp=datetime(2015, 4, 16),
+                                    title="Javascript and timing attacks",
+                                    summary="Owing to it being the only thing available in the "
+                                            "browser, people are doing more and more with Javascript...",
+                                    page_url='http://example.com/alice/javascript-timing-attacks.html',
+                                    ),
+                                ]
+            )]),
+    lambda: Blogger(name='bob',
+                    start_date=datetime(2015, 4, 1),
+                    blogs=[
+                        Blog(title='Theoretical piracy',
+                            page_url='http://example.com/bob/blog.html',
+                            feed_url='http://example.com/bob/blog.atom',
+                            posts=[
+                                Post(title="Rivest admits he doesn't care, about security, just really likes prime numbers.",
+                                    summary="""In an interview, Robert Rivest told reporters that "I'm really only into it for the math...""",
+                                    page_url='http://example.com/bob/rivest.html',
+                                    timestamp=datetime(2015, 4, 1),
+                                    ),
+                                Post(title="Real world cryptographic hash functions have no theoretical basis, and we're all doomed",
+                                    summary="We're using cryptographic hash functions like sha256 as ideal hash functions. Ideal hash "
+                                            "functions provably do not exist. This in itself isn't actually that scary, but the really "
+                                            "worrysome thing is that we don't even have a well defined notion of what's 'good enough'; "
+                                            "What's the approximation that we're actually relying on?",
+                                    page_url='http://example.com/bob/hash-functions.html',
+                                    timestamp=datetime(2015, 4, 8),
+                                    ),
+                                Post(title="Timing sidechannels ruin everything, and we have no answers whatsoever ",
+                                    summary="There's a known attack on SSH that's been around for 15 years, that allows "
+                                            "an attacker to sniff passwords by measuring the timing of a user's keystrokes --- "
+                                            "no decryption required. Nobody's done anything about it, and while we have some ideas "
+                                            "about how to deal with this one, there's a whole class of attacks out there that we "
+                                            "just have no answer for.",
+                                    page_url='http://example.com/bob/timing-sidechannels.html',
+                                    timestamp=datetime(2015, 4, 9),
+                                    ),
+                                Post(title="I've realized I'm suffering from depression.",
+                                    summary="You may have noticed my past few posts were especially dark...",
+                                    page_url='http://example.com/bob/depressed.html',
+                                    timestamp=datetime(2015, 4, 15),
+                                    ),
+                            ]
+            )]),
+]

tests/util/randomize.py
@@ -1,0 +1,42 @@
+"""This package provides helpers for randomzied testsing."""
+from random import Random
+from ironblogger.app import db
+from ironblogger.model import Blogger
+from ironblogger.date import to_dbtime
+
+
+def random_database(seed, now):
+    """Generate a random database based on random seed `seed`.
+
+    This expects to be run within an application context; after the call the
+    objects will be stored in the associated datbase.
+
+    Note that this function does not commit the changes to the database; the
+    caller must do that themselves.
+    """
+    rand = Random(seed)
+
+    blogger_choices = ['Alice', 'Bob', 'Cindy', 'Dave', 'Eve', 'Fred', 'Gina',
+                       'Henry', 'Isabelle', 'Joe', 'Kate', 'Larry', 'Mallory',
+                       'Nina', 'Owen', 'Patty', 'Quentin', 'Ron', 'Sarah',
+                       'Tim', 'Uwe', 'Vincent', 'Wendy', 'Xavier', 'Yolanda',
+                       'Zack']
+
+    num_bloggers = rand.randint(0, len(blogger_choices) - 1)
+    blogger_names = rand.sample(blogger_choices, num_bloggers)
+
+    first_start_date = now.replace(weeks=-8)
+    last_start_date = now.replace(weeks=+8)
+    for name in blogger_names:
+        start_date = random_arrow(rand, first_start_date, last_start_date)
+        db.session.add(Blogger(name=name,
+                               start_date=to_dbtime(start_date)))
+
+
+def random_arrow(rand, start, end):
+    """Generate a random arrow object in the range [start,end].
+
+    `rand` is an instance of `Random`.
+    """
+    offset = rand.randint(0, (end - start).total_seconds())
+    return start.replace(seconds=offset)