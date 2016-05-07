c536bec594cdaff9d11e4142215bab4993696e5e
Ian Denhardt <ian@zenhack.net>
2016-05-07 10:22:47 -0400

    randomized tests: generate posts

--- tests/util/randomize.py
+++ tests/util/randomize.py
@@ -2,7 +2,7 @@
 from random import Random
 from urllib import quote
 from ironblogger.app import db
-from ironblogger.model import Blogger, Blog
+from ironblogger.model import Blogger, Blog, Post
 from ironblogger.date import to_dbtime
 
 
@@ -24,6 +24,37 @@ blog_title_choices = [
     'Neat Photos',
     'Yet Another Weblog',
     'Horrible Ideas',
+]
+
+word_choices = [
+    'the', 'of', 'to', 'and', 'a', 'in', 'is', 'it', 'you', 'that', 'he',
+    'was', 'for', 'on', 'are', 'with', 'as', 'I', 'his', 'they', 'be', 'at',
+    'one', 'have', 'this', 'from', 'or', 'had', 'by', 'hot', 'but', 'some',
+    'what', 'there', 'we', 'can', 'out', 'other', 'were', 'all', 'your',
+    'when', 'up', 'use', 'word', 'how', 'said', 'an', 'each', 'she', 'which',
+    'do', 'their', 'time', 'if', 'will', 'way', 'about', 'many', 'then',
+    'them', 'would', 'write', 'like', 'so', 'these', 'her', 'long', 'make',
+    'thing', 'see', 'him', 'two', 'has', 'look', 'more', 'day', 'could',
+    'go', 'come', 'did', 'my', 'sound', 'no', 'most', 'number', 'who',
+    'over', 'know', 'water', 'than', 'call', 'first', 'people', 'may', 'down',
+    'side', 'been', 'now', 'find', 'any', 'new', 'work', 'part', 'take',
+    'get', 'place', 'made', 'live', 'where', 'after', 'back', 'little', 'only',
+    'round', 'man', 'year', 'came', 'show', 'every', 'good', 'me', 'give',
+    'our', 'under', '', 'Word', 'name', 'very', 'through', 'just', 'form',
+    'much', 'great', 'think', 'say', 'help', 'low', 'line', 'before', 'turn',
+    'cause', 'same', 'mean', 'differ', 'move', 'right', 'boy', 'old', 'too',
+    'does', 'tell', 'sentence', 'set', 'three', 'want', 'air', 'well', 'also',
+    'play', 'small', 'end', 'put', 'home', 'read', 'hand', 'port', 'large',
+    'spell', 'add', 'even', 'land', 'here', 'must', 'big', 'high', 'such',
+    'follow', 'act', 'why', 'ask', 'men', 'change', 'went', 'light', 'kind',
+    'off', 'need', 'house', 'picture', 'try', 'us', 'again', 'animal', 'point',
+    'mother', 'world', 'near', 'build', 'self', 'earth', 'father', 'head',
+    'stand', 'own', 'page', 'should', 'country', 'found', 'answer', 'school',
+    'grow', 'study', 'still', 'learn', 'plant', 'cover', 'food', 'sun',
+    'four', 'thought', 'let', 'keep', 'eye', 'never', 'last', 'door',
+    'between', 'city', 'tree', 'cross', 'since', 'hard', 'start', 'might',
+    'story', 'saw', 'far', 'sea', 'draw', 'left', 'late', 'run', "don't",
+    'while', 'press', 'close', 'night', 'real', 'life', 'few', 'stop',
 ]
 
 
@@ -68,6 +99,25 @@ def random_blogs(rand, now, blogger):
                     feed_url=feed_url)
         db.session.add(blog)
 
+
+def random_posts(rand, now, blog):
+    # The exact numbers used here are somewhat arbitrary, but the general
+    # principle is that they should slightly exceed realistic values.
+    num_posts = rand.randint(0, 42)
+    for i in range(num_posts):
+        pub_date = random_arrow(rand,
+                                now.replace(weeks=-20),
+                                now.replace(weeks=+3))
+        title = ' '.join([rand.choose(word_choices)
+                          for n in range(rand.randint(0, 15))])
+        summary = ' '.join([rand.choose(word_choices)
+                            for n in range(rand.randint(25, 150))])
+        db.session.add(Post(blog=blog,
+                            timestamp=to_dbtime(pub_date),
+                            guid='%x' % rand.getrandombits(128),
+                            page_url=blog.page_url + '/%s' % quote(title),
+                            title=title,
+                            summary=summary))
 
 
 def random_arrow(rand, start, end):
