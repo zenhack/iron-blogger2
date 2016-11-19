deedb9734e50e7fd5ae94f22622494b95349142b
Ian Denhardt <ian@zenhack.net>
2016-11-18 21:37:21 -0500

    Declare a type for format_usd

--- ironblogger/currency.py
+++ ironblogger/currency.py
@@ -1,5 +1,8 @@
+from typing import NewType
 
-def format_usd(cents):
+Cents = NewType('Cents', int)
+
+def format_usd(cents: Cents) -> str:
     """Format cents like $5.23.
 
     cents should be an amount of money in USD cents.
