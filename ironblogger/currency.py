99ec6b00d51ecf191cf40eac4feaba4d7444a103
Ian Denhardt <ian@zenhack.net>
2016-11-23 20:54:31 -0500

    Fix type annotation in ironblogger.currency

--- ironblogger/currency.py
+++ ironblogger/currency.py
@@ -1,8 +1,9 @@
 from typing import NewType
+import six
 
 Cents = NewType('Cents', int)
 
-def format_usd(cents): # type: Cents -> None
+def format_usd(cents): # type: (Cents) -> six.text_type
     """Format cents like $5.23.
 
     cents should be an amount of money in USD cents.
